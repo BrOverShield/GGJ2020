160f5abc799b325a3af5e2db7a515c806485051f
Corbeau01 <corvus-corax@hotmail.com>
2020-02-01 13:28:38 -0500

    walkman work

--- Assets/Scripts/ObjectDroper.cs
+++ Assets/Scripts/ObjectDroper.cs
@@ -14,7 +14,7 @@ public class ObjectDroper : MonoBehaviour
     {
         if(schematicView.activeSelf==true)
         {
-            if (Vector2.Distance(new Vector2(this.transform.position.x, this.transform.position.y), new Vector2(DropPoint.transform.position.x, DropPoint.transform.position.y)) <= 10)
+            if (Vector2.Distance(new Vector2(this.transform.position.x, this.transform.position.y), new Vector2(DropPoint.transform.position.x, DropPoint.transform.position.y)) <= 2)
             {
                 //DropPoint.color = Color.green;
                 this.transform.position = DropPoint.position;
