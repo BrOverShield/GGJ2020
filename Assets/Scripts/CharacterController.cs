9a0990f629bdaebd51d510fe0d9794af291f58f6
ppouch <tmdudydude@hotmail.com>
2020-02-01 11:18:08 -0500

    added force to character controller

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,20 +5,18 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     public float Speed;
+    public float thrust = 1.0f;
+
+    public float JumpThrust = 10.0f;
+    public Rigidbody rb;
     // Start is called before the first frame update
-    void Start() {
-        
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update() {
-        if (!(Input.GetKeyDown("d") && Input.GetKeyDown("a"))) {
-            if (Input.GetKey("d")) {
-                transform.Translate(Vector3.right * Speed * Time.deltaTime);
-            } else if (Input.GetKey("a")) {
-                transform.Translate(Vector3.left * Speed * Time.deltaTime);
-            }
-        }
 
         if (!(Input.GetKeyDown("q") && Input.GetKeyDown("e"))) {
             if (Input.GetKey("q")) {
@@ -26,6 +24,25 @@ public class CharacterController : MonoBehaviour
             } else if (Input.GetKey("e")) {
                 transform.Rotate(0, -1, 0);
             }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!(Input.GetKeyDown("d") && Input.GetKeyDown("a"))) {
+            if (Input.GetKey("d")) {
+                transform.Translate(Vector3.right * Speed * Time.deltaTime);
+                rb.AddForce(Vector3.right * thrust);
+            } else if (Input.GetKey("a")) {
+                transform.Translate(Vector3.left * Speed * Time.deltaTime);
+                rb.AddForce(Vector3.left * thrust);
+            }
+        }
+        print(rb.velocity.y);
+        if (Input.GetKeyDown("space") && Mathf.Abs(rb.velocity.y) < 0.5f)
+        {
+            print("yeah");
+            rb.AddForce(Vector3.up * JumpThrust);
         }
     }
 }
