4822979bd6d0747e40e12ce21414871a1e605521
panoramicdream <looking4astonauts@gmail.com>
2020-02-01 16:54:45 -0500

    schematicViews.cs

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -6,13 +6,22 @@ public class Buttons : MonoBehaviour
 {
     int myListId;
     bluePrints bp;
+
     private void Start()
     {
         bp = FindObjectOfType<bluePrints>();
     }
     public void b1()
     {
-        
+
+        print("hello");
+
     }
-       
+
+    public void loadNextSchematic()
+    {
+
+    }
+
 }
+
---
+++ Assets/Scripts/aSchematic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aSchematic : MonoBehaviour
+{
+
+    public int myParts = 0;
+    public int maxParts = 0; 
+
+    [SerializeField]
+    public bluePrints.bluePrintsEnum thisBluePrint;
+
+    void Start()
+    {
+        switch (thisBluePrint)
+        {
+            case bluePrints.bluePrintsEnum.Walkman :
+                maxParts = 6;
+                break;
+            case bluePrints.bluePrintsEnum.Ladder:
+                maxParts = 4;
+                break;
+            case bluePrints.bluePrintsEnum.Cart:
+                maxParts = 3;
+                break;
+         }       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
