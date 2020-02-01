10061e3342acbf8b74117e8a2423a4241d237c1e
Corbeau01 <corvus-corax@hotmail.com>
2020-02-01 11:51:00 -0500

    inventory

---
+++ Assets/ObjectDroper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectDroper : MonoBehaviour
+{
+    public Image DropPoint;
+    public GameObject schematicView;
+    //if is drop over a drop point.
+    //Destroy
+    //Activate slot
+    private void OnMouseUp()
+    {
+        if(schematicView.activeSelf==true)
+        {
+            if (Vector2.Distance(new Vector2(this.transform.position.x, this.transform.position.y), new Vector2(DropPoint.transform.position.x, DropPoint.transform.position.y)) <= 1)
+            {
+                DropPoint.color = Color.green;
+                Destroy(this.gameObject);
+            }
+        }
+        
+    }
+}
